7bc9704ccc61ffe5f3c297315a9dc7a5715f0349
Nicholas Simarmata <nicholas.simarmata@gmail.com>
2026-04-20 09:09:40 +0700

    ganti max_sum 2

--- soal1.c
+++ soal1.c
@@ -26,7 +26,7 @@ void display(int array[], int size){
     for(int i = 0; i < size; i++){
         printf("%d", array[i]);
         if (i != size-1) printf(" ");
-        if(array[i] > 0) sum += array[i];
+        if(array[i] >= 0) sum += array[i];
     }
     printf("\nMAX_SUM %d", sum);
 }
